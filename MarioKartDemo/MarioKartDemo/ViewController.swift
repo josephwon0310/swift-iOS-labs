8e1f04a0f499e0ea8b4db18b21937b83eef94972
josephwon0310 <josephwon0310@gmail.com>
2020-02-12 18:39:10 -0500

    lab4

---
+++ MarioKartDemo/MarioKartDemo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MarioKartDemo
+//
+//  Created by Joseph Won on 2/12/20.
+//  Copyright © 2020 Joseph Won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        print("Location: x: \(location.x), y: \(location.y)")
+        let kartView = sender.view!
+        kartView.center = location
+        
+
+    }
+    
+
+}
+
