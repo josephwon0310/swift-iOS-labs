bd9a846d5939ed514086d467bd1162a3e4d6b19d
josephwon0310 <josephwon0310@gmail.com>
2020-02-12 19:20:24 -0500

    pinch, pan, rotate, double tap, long press

--- MarioKartDemo/MarioKartDemo/ViewController.swift
+++ MarioKartDemo/MarioKartDemo/ViewController.swift
@@ -9,21 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var kartView0: UIImageView!
+    @IBOutlet weak var kartView1: UIImageView!
+    @IBOutlet weak var kartView2: UIImageView!
+    
+    var startingPointKartView0 = CGPoint()
+    var startingPointKartView1 = CGPoint()
+    var startingPointKartView2 = CGPoint()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startingPointKartView0 = kartView0.center
+        startingPointKartView1 = kartView1.center
+        startingPointKartView2 = kartView2.center
         // Do any additional setup after loading the view.
     }
     
     @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        
         let location = sender.location(in: view)
-        print("Location: x: \(location.x), y: \(location.y)")
+        //print("Location: x: \(location.x), y: \(location.y)")
         let kartView = sender.view!
         kartView.center = location
-        
-
     }
     
-
+    @IBAction func didPinchKart(_ sender: UIPinchGestureRecognizer) {
+        
+        let scale = sender.scale
+        //print("scale: \(scale)")
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    
+    
+    @IBAction func didRotateKartView(_ sender: UIRotationGestureRecognizer) {
+        
+        let rotation = sender.rotation
+        //print("rotation: \(rotation)")
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(rotationAngle: rotation)
+    }
+    
+    @IBAction func didTapKart(_ sender: UITapGestureRecognizer) {
+        //let location = sender.location(in: view)
+        //print("location: \(location)")
+        let kartView = sender.view!
+        
+        UIView.animate(withDuration: 0.8) {
+            kartView.center.x += 400
+        }
+    }
+    
+    @IBAction func didLongPress(_ sender: UILongPressGestureRecognizer) {
+//        kartView0.center = startingPointKartView0
+//        kartView1.center = startingPointKartView1
+//        kartView2.center = startingPointKartView2
+        
+        UIView.animate(withDuration: 0.8) {
+              self.kartView0.center = self.startingPointKartView0
+              self.kartView1.center = self.startingPointKartView1
+              self.kartView2.center = self.startingPointKartView2
+        }
+    }
+    
 }
 
